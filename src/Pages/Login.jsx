90441cf8d2d6bced2fbc42337c6ba427dd4b1252
Oscar Meneses <omenesess1800@alumno.ipn.mx>
2023-07-10 09:34:47 -0600

    add notifications to error and sucess login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,11 +2,37 @@ import '../Styles/Login.css'
 import data from '../DataBase/login.json'
 import md5 from 'md5'
 import logo from '../Images/logo-celtics.png'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const Login = () => {
 
+    const notifySuccess = () => {
+        toast.success('Login exitoso', {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+        });
+    }
 
+    const notifyError = () => {
+        toast.error('Usuario o contraseña incorrectos', {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+        });
+    }
 
     const handleEnter = addEventListener('keypress', (e) => {
         if (e.key === 'Enter') {
@@ -17,25 +43,36 @@ const Login = () => {
     const handleLogin = () => {
         const username = document.getElementById('username').value
         const password = document.getElementById('password').value
-        const message = document.querySelector('.inactive')
         const user = data.users.find(user => user.username === username && user.password === md5(password))
         if (user) {
             localStorage.setItem('user', JSON.stringify(user))
             if (user.perfil === 'admin') {
-                window.location.href = './home'
+                setTimeout(() => {
+                    window.location.href = '/home'
+                }, 1200)
+                notifySuccess()
             }
             if (user.perfil === 'socio') {
-                window.location.href = './ventas'
+                setTimeout(() => {
+                    window.location.href = '/ventas'
+                }, 1200)
+                notifySuccess()
             }
             if (user.perfil === 'cajero') {
-                window.location.href = './caja'
+                setTimeout(() => {
+                    window.location.href = '/caja'
+                }, 1200)
+                notifySuccess()
             }
             if (user.perfil === 'mesero') {
-                window.location.href = './iventario'
+                setTimeout(() => {
+                    window.location.href = '/inventario'
+                }, 1200)
+                notifySuccess()
             }
         }
         else {
-            message.classList.remove('inactive')
+            notifyError()
         }
     }
 
@@ -59,7 +96,7 @@ const Login = () => {
                     className="mt-3 px-3 py-4 border border-solid rounded-lg border-black bg-white"
                     required
                 />
-                <a className='message inactive animate-pulse'>¡Usuario o contraseña incorrectos!</a>
+                <ToastContainer />
                 <button
                     type="submit"
                     className="bg-green-950/90 px-4 py-3 mt-7 mb-10 rounded-md text-white font-bold"
